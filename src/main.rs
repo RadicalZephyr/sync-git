0f00ad9477c3e117173ee24e379d88081dc04b63
Geoff Shannon <geoffpshannon@gmail.com>
2019-01-09 13:51:05 -0800

    Report on not-current files in all repositories

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use failure::Fail;
-use git2::Repository;
+use git2::{Repository, Status, StatusOptions};
 use walkdir::{DirEntry, IntoIter, WalkDir};
 
 #[derive(Debug, Fail)]
@@ -73,10 +73,29 @@ impl Iterator for GitRepos<IntoIter> {
     }
 }
 
+fn is_clean(repo: &Repository) -> bool {
+    match repo.state() {
+        git2::RepositoryState::Clean => true,
+        _ => false,
+    }
+}
+
 fn main() -> Result<()> {
+    let mut status_opts = StatusOptions::new();
+    status_opts.include_ignored(false).include_untracked(true);
+
     let walker = WalkDir::new(".").into_iter();
-    for repo in GitRepos::new(walker).filter_map(Result::ok) {
+    for repo in GitRepos::new(walker)
+        .filter_map(Result::ok)
+        .filter(is_clean)
+    {
         println!("{}", repo.path().display());
+
+        for status in repo.statuses(Some(&mut status_opts))?.iter() {
+            if status.status() != Status::CURRENT {
+                println!("  file is not current: {}", status.path().unwrap());
+            }
+        }
     }
     Ok(())
 }
