10e28c76ddc53a08a6706a025bc038ac5162ef9c
Geoff Shannon <geoffpshannon@gmail.com>
2019-01-12 13:59:43 -0800

    Move main code into lib.rs file

---
+++ src/lib.rs
@@ -0,0 +1,84 @@
+use failure::Fail;
+use git2::Repository;
+use walkdir::{DirEntry, IntoIter};
+
+#[derive(Debug, Fail)]
+pub enum Error {
+    #[fail(display = "walkdir error: {}", _0)]
+    WalkDir(#[cause] walkdir::Error),
+
+    #[fail(display = "git2 error: {}", _0)]
+    Git2(#[cause] git2::Error),
+}
+
+impl From<git2::Error> for Error {
+    fn from(err: git2::Error) -> Error {
+        Error::Git2(err)
+    }
+}
+
+impl From<walkdir::Error> for Error {
+    fn from(err: walkdir::Error) -> Error {
+        Error::WalkDir(err)
+    }
+}
+
+pub type Result<T> = std::result::Result<T, Error>;
+
+fn is_git_dir(entry: &DirEntry) -> bool {
+    entry
+        .file_name()
+        .to_str()
+        .map(|s| s.ends_with(".git"))
+        .unwrap_or(false)
+}
+
+pub struct GitRepos<I> {
+    it: I,
+}
+
+impl GitRepos<IntoIter> {
+    pub fn new(it: IntoIter) -> GitRepos<IntoIter> {
+        GitRepos { it }
+    }
+}
+
+/// Like try, but for iterators that return [`Option<Result<_, _>>`].
+///
+/// [`Option<Result<_, _>>`]: https://doc.rust-lang.org/stable/std/option/enum.Option.html
+macro_rules! itry {
+    ($e:expr) => {
+        match $e {
+            Ok(v) => v,
+            Err(err) => return Some(Err(From::from(err))),
+        }
+    };
+}
+
+impl Iterator for GitRepos<IntoIter> {
+    type Item = Result<Repository>;
+
+    fn next(&mut self) -> Option<Result<Repository>> {
+        loop {
+            let dent = match self.it.next() {
+                Some(dent) => itry!(dent),
+                None => return None,
+            };
+            if dent.file_type().is_dir() && is_git_dir(&dent) {
+                self.it.skip_current_dir();
+                let repo = itry!(Repository::open(dent.path()));
+                return Some(Ok(repo));
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_thing() {
+        assert_eq!(true, false);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,77 +1,7 @@
-use failure::Fail;
 use git2::{Repository, Status, StatusOptions};
-use walkdir::{DirEntry, IntoIter, WalkDir};
+use walkdir::WalkDir;
 
-#[derive(Debug, Fail)]
-pub enum Error {
-    #[fail(display = "walkdir error: {}", _0)]
-    WalkDir(#[cause] walkdir::Error),
-
-    #[fail(display = "git2 error: {}", _0)]
-    Git2(#[cause] git2::Error),
-}
-
-impl From<git2::Error> for Error {
-    fn from(err: git2::Error) -> Error {
-        Error::Git2(err)
-    }
-}
-
-impl From<walkdir::Error> for Error {
-    fn from(err: walkdir::Error) -> Error {
-        Error::WalkDir(err)
-    }
-}
-
-type Result<T> = std::result::Result<T, Error>;
-
-fn is_git_dir(entry: &DirEntry) -> bool {
-    entry
-        .file_name()
-        .to_str()
-        .map(|s| s.ends_with(".git"))
-        .unwrap_or(false)
-}
-
-struct GitRepos<I> {
-    it: I,
-}
-
-impl GitRepos<IntoIter> {
-    pub fn new(it: IntoIter) -> GitRepos<IntoIter> {
-        GitRepos { it }
-    }
-}
-
-/// Like try, but for iterators that return [`Option<Result<_, _>>`].
-///
-/// [`Option<Result<_, _>>`]: https://doc.rust-lang.org/stable/std/option/enum.Option.html
-macro_rules! itry {
-    ($e:expr) => {
-        match $e {
-            Ok(v) => v,
-            Err(err) => return Some(Err(From::from(err))),
-        }
-    };
-}
-
-impl Iterator for GitRepos<IntoIter> {
-    type Item = Result<Repository>;
-
-    fn next(&mut self) -> Option<Result<Repository>> {
-        loop {
-            let dent = match self.it.next() {
-                Some(dent) => itry!(dent),
-                None => return None,
-            };
-            if dent.file_type().is_dir() && is_git_dir(&dent) {
-                self.it.skip_current_dir();
-                let repo = itry!(Repository::open(dent.path()));
-                return Some(Ok(repo));
-            }
-        }
-    }
-}
+use sync_git::{GitRepos, Result};
 
 fn is_clean(repo: &Repository) -> bool {
     match repo.state() {
